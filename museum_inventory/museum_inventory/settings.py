542a78481b946f4a667e7083282d1638dd6b9b6d
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-10-10 16:17:55 -0400

    Configured Database in settings.py.

--- museum_inventory/museum_inventory/settings.py
+++ museum_inventory/museum_inventory/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'museum_inventory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'museum_inventory',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
     }
 }
 
